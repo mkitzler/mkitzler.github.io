9ef5880248a1b61fcda2b5b00072ad1ea8946cdd
m_kitzler <martin.kitzler62@gmail.com>
2021-09-27 20:59:53 +0200

    Initial Commit

---
+++ Blazor_TicTacToe/Blazor_TicTacToe/Client/wwwroot/js/interop.js
@@ -0,0 +1,11 @@
+﻿function showModal(modalId) {
+    $("#" + modalId).modal("show")
+}
+
+function hideModal(modalId) {
+    $("#" + modalId).modal("hide")
+}
+
+function toggleModal(modalId) {
+    $("#" + modalId).modal("toggle")
+}
