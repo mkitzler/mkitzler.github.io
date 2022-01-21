c0d901a06cc55a49635434e265e1c5560a2795a2
Martin Kitzler <m.kitzler@htlkrems.at>
2022-01-21 11:41:03 +0100

    Added DVD Logo, some logic

---
+++ DVD/DVD/wwwroot/js/interop.js
@@ -0,0 +1,20 @@
+﻿window.getDimensions = function () {
+    return {
+        width: window.innerWidth,
+        height: window.innerHeight
+    };
+};
+
+window.getDVDDimensions = function () {
+    let dvd = document.getElementById("dvd");
+    console.log(dvd);
+    if (dvd) {
+        return {
+            width: dvd.width,
+            height: dvd.height
+        };
+    }
+    else {
+        return null;
+    }
+};
