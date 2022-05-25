fe7d00407358d75a39af0b2cece9699bfd93e223
mkitzler <martin.kitzler62@gmail.com>
2022-05-25 15:03:39 +0200

    Added BTree Page

---
+++ GithubPages/BTreeLibrary/BTree.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BTreeLibrary
+{
+    internal class BTree<T> where T : IComparable<T>
+    {
+        private int _maxSize;
+        public List<BTreeNode<T>> Subnodes { get; private set; }
+
+        public BTree(int maxSize)
+        {
+            _maxSize = maxSize;
+            Subnodes = new List<BTreeNode<T>>();
+        }
+        public BTree(int maxSize, T value) : this(maxSize)
+        {
+            Subnodes.Add(new BTreeNode<T>(_maxSize, value));
+        }
+
+        public int Depth()
+        {
+            if (Subnodes.Count > 0)
+                return Subnodes[0].Depth() + 1;
+            else
+                return 0;
+        }
+
+        public void Add(T value)
+        {
+            //Add / Insert
+            int addAt = 0;
+            for (; addAt < Subnodes.Count && value.CompareTo(Subnodes[addAt].Value) >= 0; addAt++) { }
+            if (addAt < Subnodes.Count && Subnodes[addAt].HasSubnodes)
+            {
+                List<BTreeNode<T>>? ret = Subnodes[addAt].Add(value);
+                if (ret != null)
+                {
+                    Subnodes.Insert(addAt, new BTreeNode<T>(_maxSize, ret));
+                }
+            }
+            else
+            {
+                Subnodes.Insert(addAt, new BTreeNode<T>(_maxSize, value));
+            }
+
+            // Split
+            if (Subnodes.Count > _maxSize)
+            {
+                int firstHalfCount = (int)Math.Floor(Subnodes.Count / 2d);
+                List<BTreeNode<T>>? firstH = Subnodes.GetRange(0, firstHalfCount);
+                Subnodes.RemoveRange(0, firstHalfCount);
+                List<BTreeNode<T>>? secondH = Subnodes.GetRange(0, Subnodes.Count - firstHalfCount);
+                Subnodes.RemoveRange(0, Subnodes.Count - firstHalfCount);
+                Subnodes.Add(new BTreeNode<T>(_maxSize, firstH));
+                Subnodes.Add(new BTreeNode<T>(_maxSize, secondH));
+            }
+        }
+
+        public List<List<T[]>> ToList()
+        {
+            List<List<T[]>> ret = new List<List<T[]>>();
+            foreach (var node in Subnodes)
+            {
+                node.ToList(ret);
+            }
+            return ret;
+        }
+
+        public void ForEach(Action<BTreeNode<T>, int> action)
+        {
+            Subnodes.ForEach(x => x.ForEach(action, 0));
+        }
+    }
+}
---
+++ GithubPages/BTreeLibrary/BTreeNode.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BTreeLibrary
+{
+    internal class BTreeNode<T> where T : IComparable<T>
+    {
+        private int _maxSize;
+        public bool HasSubnodes {
+            get
+            {
+                return Subnodes.Count > 0;
+            }
+        }
+        public T Value { get; private set; }
+        public List<BTreeNode<T>> Subnodes { get; private set; }
+
+
+        public BTreeNode(int maxSize, T value)
+        {
+            _maxSize = maxSize;
+            Value = value;
+            Subnodes = new List<BTreeNode<T>>();
+        }
+        public BTreeNode(int maxSize, List<BTreeNode<T>> list) : this(maxSize, list.Last().Value)
+        {
+            Subnodes = list;
+        }
+
+        public int Depth()
+        {
+            if (HasSubnodes)
+                return Subnodes[0].Depth() + 1;
+            else
+                return 0;
+        }
+
+
+        public List<BTreeNode<T>>? Add(T value)
+        {
+            //Add / Insert
+            int addAt = 0;
+            for (; addAt < Subnodes.Count && value.CompareTo(Subnodes[addAt].Value) >= 0; addAt++) {}
+            if (addAt < Subnodes.Count && Subnodes[addAt].HasSubnodes)
+            {
+                List<BTreeNode<T>>? ret = Subnodes[addAt].Add(value);
+                if (ret != null)
+                {
+                    Subnodes.Insert(addAt, new BTreeNode<T>(_maxSize, ret));
+                }
+            }
+            else
+            {
+                Subnodes.Insert(addAt, new BTreeNode<T>(_maxSize, value));
+            }
+
+            // Split
+            if (Subnodes.Count > _maxSize)
+            {
+                int firstHalfCount = (int)Math.Floor(Subnodes.Count / 2d);
+                List<BTreeNode<T>>? ret = Subnodes.GetRange(0, firstHalfCount);
+                Subnodes.RemoveRange(0, firstHalfCount);
+                Value = Subnodes.Last().Value;
+                return ret;
+            }
+            else
+            {
+                return null;
+            }
+        }
+
+        public int ToList(List<List<T[]>> valueCounts)
+        {
+            int level = 0;
+            if (Subnodes[0].HasSubnodes)
+            {
+                foreach(BTreeNode<T> node in Subnodes)
+                {
+                    level = node.ToList(valueCounts);
+                }
+            }
+
+            if (valueCounts.Count <= level)
+            {
+                valueCounts.Add(new List<T[]>());
+            }
+            valueCounts[level].Add(Subnodes.Select(x => x.Value).ToArray());
+
+            return level + 1;
+        }
+
+        public void ForEach(Action<BTreeNode<T>, int> action, int level)
+        {
+            Subnodes.ForEach(x => x.ForEach(action, 0));
+        }
+    }
+}
